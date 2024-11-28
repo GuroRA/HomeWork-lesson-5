538ac314e954c7e5c6079a7202f4300db9f174fd
firman <rus.guro123@gmail.com>
2024-11-28 19:31:58 +0300

    Добавьте файлы проекта.

---
+++ File-leson-5/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace File_leson_5
+{
+    /// <summary>
+    /// Предметы ЕГЭ (информатика, англ, физика)
+    /// </summary>
+    public enum Subjects
+    {
+        Informatics,
+        English,
+        Physics,
+    }
+    class Student
+    {
+        public string LastName;
+        public string FirstName;
+        public DateTime BirthDay;
+        public Subjects Subject;
+        public int Score;
+
+
+        public Student(string lastName, string firstName, string d, Subjects subject, int score)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            BirthDay = DateTime.Parse(d);
+            Subject = subject;
+            Score = score;
+        }
+    }
+
+
+    internal class Program
+    {
+        /// <summary>
+        /// Создаёт список ссылок на картинки и рандомизирует его
+        /// </summary>
+        static void GetRandomListOrder()
+        {
+            List<string> images = new List<string> {
+                "image1.jpg", "image1.jpg",
+                "image2.jpg", "image2.jpg",
+                "image3.jpg", "image3.jpg",
+                "image4.jpg", "image4.jpg",
+                "image5.jpg", "image5.jpg",
+                "image6.jpg", "image6.jpg",
+                "image7.jpg", "image7.jpg",
+                "image8.jpg", "image8.jpg",
+                "image9.jpg", "image9.jpg",
+                "image10.jpg", "image10.jpg",
+                "image11.jpg", "image11.jpg",
+                "image12.jpg", "image12.jpg",
+                "image13.jpg", "image13.jpg",
+                "image14.jpg", "image14.jpg",
+                "image15.jpg", "image15.jpg",
+                "image16.jpg", "image16.jpg",
+                "image17.jpg", "image17.jpg",
+                "image18.jpg", "image18.jpg",
+                "image19.jpg", "image19.jpg",
+                "image20.jpg", "image20.jpg",
+                "image21.jpg", "image21.jpg",
+                "image22.jpg", "image22.jpg",
+                "image23.jpg", "image23.jpg",
+                "image24.jpg", "image24.jpg",
+                "image25.jpg", "image25.jpg",
+                "image26.jpg", "image26.jpg",
+                "image27.jpg", "image27.jpg",
+                "image28.jpg", "image28.jpg",
+                "image29.jpg", "image29.jpg",
+                "image30.jpg", "image30.jpg",
+                "image31.jpg", "image31.jpg",
+                "image32.jpg", "image32.jpg",
+            };
+
+            Console.WriteLine("Порядок картинок до перемешивания: ");
+            images.ForEach(p => Console.WriteLine(p));
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < images.Count; i++)
+            {
+                string tmp = images[0];
+                images.RemoveAt(0);
+                images.Insert(rnd.Next(images.Count), tmp);
+
+            }
+            Console.WriteLine("\nПорядок картинок после перемешивания: ");
+            images.ForEach(p => Console.WriteLine(p));
+            return;
+        }
+
+        /// <summary>
+        /// Список судентов
+        /// </summary>
+        /// <returns></returns>
+        static Dictionary<int, Student> GetStudents()
+        {
+            Dictionary<int, Student> students = new Dictionary<int, Student>()
+            {
+                { 1, new Student( "Гуро", "Руслан", "17.09.2006", Subjects.Informatics, 242) },
+                { 2, new Student( "Балобанова", "Арина", "13.03.2006", Subjects.English, 247) },
+                { 3, new Student( "Валеев", "Сергей", "25.08.2006", Subjects.Physics, 278) },
+                { 4, new Student( "Габов", "Алексей", "05.07.2006", Subjects.Informatics, 252) },
+                { 5, new Student( "Дорожан", "Андрей", "14.05.2006", Subjects.Informatics, 255) },
+                { 6, new Student( "Лисюткина", "Руслана", "28.06.2006", Subjects.Informatics, 241) },
+                { 7, new Student( "Назарова", "Дарья", "11.05.2006", Subjects.English, 239) },
+                { 8, new Student( "Сабирова", "Далия", "14.05.2006", Subjects.English, 287) },
+                { 9, new Student( "Спиридонова", "Весна", "15.04.2006", Subjects.Physics, 253) },
+                { 10, new Student( "Халикова", "Данна", "03.11.2006", Subjects.English, 262) },
+            };
+            return students;
+
+        }
+        static void Main(string[] args)
+        {
+            //Создать List на 64 элемента, скачать из интернета 32 пары картинок (любых). В List
+            //должно содержаться по 2 одинаковых картинки. Необходимо перемешать List с
+            //картинками.Вывести в консоль перемешанные номера(изначальный List и полученный
+            //List).Перемешать любым способом.
+            Console.WriteLine("Задание 1");
+            GetRandomListOrder();
+
+            //Создать студента из вашей группы (фамилия, имя, год рождения, с каким экзаменом
+            //поступил, баллы). Создать словарь для студентов из вашей группы(10 человек).
+            //Необходимо прочитать информацию о студентах с файла.В консоли необходимо создать
+            //меню:
+            //a.Если пользователь вводит: Новый студент, то необходимо ввести
+            //информацию о новом студенте и добавить его в List
+            //b.Если пользователь вводит: Удалить, то по фамилии и имени удаляется
+            //студент
+            //c.Если пользователь вводит: Сортировать, то происходит сортировка по баллам
+            //(по возрастанию)
+            Console.WriteLine("\nЗадание 2");
+
+            Dictionary<int, Student> students = GetStudents();
+            Console.WriteLine("Существующий список студентов: ");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key}: {student.Value.FirstName} {student.Value.LastName} {student.Value.Subject} {student.Value.Score}");
+            }
+            Console.WriteLine("\nЕсли вы хотите добавить нового пользователя введите: Новый студент\nЕсли вы хотите удалить пользователя введите: Удалить\nЕсли вы хотите отсортировать пользователей по балам введите: Отсортировать");
+
+            switch (Console.ReadLine().ToLower())
+            {
+                case "новый студент":
+                    students.Add(students.Count+1, new Student("Мустафин", "Олег", "12.12.2006", Subjects.English, 234));
+                    break;
+                case "удалить":
+                    Console.WriteLine("\nВведите имя и фамилию студента которого хотите удалить: ");
+                    string firstNameToRemove = Console.ReadLine().ToLower();
+                    string LastNameToRemove = Console.ReadLine().ToLower();
+                    students.Remove(students.FirstOrDefault(x => x.Value.FirstName.ToLower() == firstNameToRemove && x.Value.LastName.ToLower() == LastNameToRemove).Key);
+                    break;
+                case "отсортировать":
+                    students = students.OrderBy(d => -d.Value.Score).ToDictionary(d => d.Key, d => d.Value);
+                    break;
+
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key}: {student.Value.FirstName} {student.Value.LastName} {student.Value.Subject} {student.Value.Score}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tymakov-lesson-5/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tymakov_lesson_5
+{
+
+    enum Month
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        Jule,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    internal class Program
+    {
+
+        /// <summary>
+        /// Возвращает масив первый элемент - кол-во гласных, второй - кол-во согласных
+        /// </summary>
+        /// <param name="fileText"></param>
+        /// <returns></returns>
+        static int[] GetVowConCount(List<char> TextChars)
+        {
+            string vowels = "аеёиоуыэюя";
+            int vowelsCount = 0;
+            int consonantCount = 0;
+
+            foreach (char letter in TextChars)
+            {
+                if (char.IsLetter(letter))
+                {
+                    if (vowels.Contains(char.ToLower(letter)))
+                    {
+                        vowelsCount++;
+                    }
+                    else
+                    {
+                        consonantCount++;
+                    }
+                }
+            }
+            return [vowelsCount, consonantCount];
+        }
+
+        /// <summary>
+        /// Считывает текст с уже созданного файла
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        static List<char> GetTextOfFile(string fileName)
+        {
+            using FileStream file = File.OpenRead(fileName);
+            byte[] array = new byte[file.Length];
+            file.Read(array, 0, array.Length);
+
+            char[] chars = System.Text.Encoding.UTF8.GetString(array).ToCharArray();
+
+            //Зачем в этой задаче использовать лист когда я могу передать сам массив символов в функцию я не понимаю
+            List<char> list = [.. chars];
+            return list;
+        }
+
+        /// <summary>
+        /// Числа в матрице не превышают 10
+        /// </summary>
+        /// <param name="rows"></param>
+        /// <param name="cols"></param>
+        /// <returns></returns>
+        static int[,] GetMatrix2D(uint rows, uint cols)
+        {
+            Random rnd = new Random();
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rnd.Next(-20, 35);
+                }
+            }
+            return matrix;
+        }
+
+        static int[,] MultiplayMatrixs(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            {
+                Console.WriteLine("Мы не можем перемножить эти матрицы");
+                return null;
+            }
+            else
+            {
+                int temp = 0;
+                int[,] multiplayed = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+                for (int row = 0; row < multiplayed.GetLength(0); row++)
+                {
+                    for (int col = 0; col < multiplayed.GetLength(1); col++)
+                    {
+                        temp = 0;
+                        for (int i = 0; i < matrix1.GetLength(1); i++)
+                        {
+                            temp += matrix1[row, i] * matrix2[i, col];
+                        }
+                        multiplayed[row, col] = temp;
+                    }
+                }
+                return multiplayed;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Написать программу, которая вычисляет число гласных
+            //и согласных букв в файле.Имя файла передавать как аргумент в функцию
+            //Main.Содержимое текстового файла заносится в массив символов.Количество
+            //гласных и согласных букв определяется проходом по массиву.Предусмотреть
+            //метод, входным параметром которого является массив символов.Метод
+            //вычисляет количество гласных и согласных букв.
+
+            Console.WriteLine("Упражнение 6.1");
+            int[] vowConCounters = GetVowConCount(GetTextOfFile("text.txt"));
+            Console.WriteLine($"Кол-во гласных букв: {vowConCounters[0]}\nКол-во согласных букв: {vowConCounters[1]}");
+
+            Console.WriteLine("\nУпражнение 6.2");
+            int[,] multiplayedMatrix = MultiplayMatrixs(GetMatrix2D(2, 2), GetMatrix2D(2, 3));
+            if (multiplayedMatrix != null)
+            {
+                for (int row = 0; row < multiplayedMatrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < multiplayedMatrix.GetLength(1); col++)
+                    {
+                        Console.Write(multiplayedMatrix[row, col] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
+            //Написать программу, вычисляющую среднюю температуру за год. Создать
+            //двумерный рандомный массив temperature[12, 30], в котором будет храниться температура
+            //для каждого дня месяца(предполагается, что в каждом месяце 30 дней).Сгенерировать
+            //значения температур случайным образом. Для каждого месяца распечатать среднюю
+            //температуру.Для этого написать метод, который по массиву temperature[12, 30] для каждого
+            //месяца вычисляет среднюю температуру в нем, и в качестве результата возвращает массив
+            //средних температур.Полученный массив средних температур отсортировать по
+            //возрастанию.
+
+            Console.WriteLine("\nУпражнение 6.3");
+
+            Dictionary<Month, double> temperatures = new Dictionary<Month, double>();
+
+
+            int[,] yearTemperature = GetMatrix2D(12, 30);
+
+            for (int row = 0; row < yearTemperature.GetLength(0); row++)
+            {
+                double sum = 0;
+                for (int col = 0; col < yearTemperature.GetLength(1); col++)
+                {
+                    sum += yearTemperature[row, col];
+                }
+                temperatures.Add((Month)row, (double)sum / yearTemperature.GetLength(1));
+            }
+
+            foreach (var item in temperatures)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value:F1}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
